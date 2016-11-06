078e6f102d7a94937498d1daa727ec87a4a247ea
Samintha Kaveesh <skaveesh@gmail.com>
2016-11-06 09:52:21 +0530

    converted to local database

--- src/wijesekara/stores/pos/LoginWindow.java
+++ src/wijesekara/stores/pos/LoginWindow.java
@@ -5,6 +5,16 @@
  */
 package wijesekara.stores.pos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileSystemView;
+
 /**
  *
  * @author Samintha
@@ -14,8 +24,52 @@ public class LoginWindow extends javax.swing.JFrame {
     /**
      * Creates new form LoginWindow
      */
+    JFileChooser fr = new JFileChooser();
+    FileSystemView fw = fr.getFileSystemView();
+    Base64Converter base64 = new Base64Converter();
+    ConfigureDBWindow config_window;
+    
+    
     public LoginWindow() {
         initComponents();
+        
+        
+        
+        //this belowe code working if config file in user documents not exists.
+        File file = new File("config.dat");
+        File file_in_library_ms_dir = new File(fw.getDefaultDirectory() + "\\WPOS\\config.dat");
+        File library_ms_dir = new File(fw.getDefaultDirectory() + "\\WPOS");
+        if (!library_ms_dir.exists() | !file_in_library_ms_dir.exists()) {
+            try {
+                library_ms_dir.mkdir();
+            } catch (SecurityException ex) {
+                JOptionPane.showMessageDialog(null, "Cannot create a folder in your document, because of security of your system");
+            }
+            
+            try {
+                
+                FileOutputStream writeStream = new FileOutputStream(file, false); // true to append
+                PrintWriter pw = new PrintWriter(writeStream);
+                pw.println("");
+                pw.println("root");
+                pw.println("");
+                pw.println("localhost");
+                pw.println("wijesekarapos");
+                pw.flush();
+                pw.close();
+
+                base64.encode_to_base64(); //convert temporary file to Base64
+                file.delete(); //delete temporary created file
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "Error writting to config file!");
+            }
+        }
+        
+        view_config_window();
+    }
+    
+    public void view_config_window(){
+        config_window =  new ConfigureDBWindow();
     }
 
     /**
@@ -32,10 +86,11 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        login_btn = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        configdbBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,8 +100,13 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Calibri Light", 0, 12)); // NOI18N
         jLabel2.setText("Password");
 
-        jButton1.setFont(new java.awt.Font("Calibri Light", 0, 11)); // NOI18N
-        jButton1.setText("LOGIN");
+        login_btn.setFont(new java.awt.Font("Calibri Light", 0, 11)); // NOI18N
+        login_btn.setText("LOGIN");
+        login_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                login_btnActionPerformed(evt);
+            }
+        });
 
         jLabel3.setFont(new java.awt.Font("Calibri Light", 0, 36)); // NOI18N
         jLabel3.setText("Wijesekara Stores");
@@ -56,6 +116,13 @@ public class LoginWindow extends javax.swing.JFrame {
 
         jLabel5.setFont(new java.awt.Font("Calibri Light", 0, 11)); // NOI18N
         jLabel5.setText("Copyright © 2106 All Rights Reserved.");
+
+        configdbBtn.setText("Configure Database");
+        configdbBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                configdbBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -71,7 +138,7 @@ public class LoginWindow extends javax.swing.JFrame {
                                 .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
                                 .addGroup(jPanel1Layout.createSequentialGroup()
                                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(jButton1)
+                                        .addComponent(login_btn)
                                         .addGroup(jPanel1Layout.createSequentialGroup()
                                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                                 .addComponent(jLabel1)
@@ -80,11 +147,14 @@ public class LoginWindow extends javax.swing.JFrame {
                                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                                 .addComponent(jTextField1)
                                                 .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                                    .addGap(16, 16, 16)))))
+                                    .addGap(16, 16, 16))))
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel5)))
-                .addContainerGap(70, Short.MAX_VALUE))
+                        .addComponent(jLabel5)
+                        .addGap(76, 76, 76)
+                        .addComponent(configdbBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,18 +163,24 @@ public class LoginWindow extends javax.swing.JFrame {
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(jLabel4)
-                .addGap(33, 33, 33)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(31, 31, 31)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-                .addComponent(jLabel5))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(31, 31, 31)
+                        .addComponent(login_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                        .addComponent(jLabel5))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(configdbBtn)
+                        .addContainerGap())))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -120,6 +196,47 @@ public class LoginWindow extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void configdbBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_configdbBtnActionPerformed
+        ConfigureDBWindow configdb = new ConfigureDBWindow();
+        configdb.pack();
+        configdb.setLocationRelativeTo(null);
+        configdb.setVisible(true);
+    }//GEN-LAST:event_configdbBtnActionPerformed
+
+    private void login_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_login_btnActionPerformed
+        String username_db = "", password_db = "";
+
+        String line, mysql_uname = "root", mysql_pass = "", mysql_server = "localhost", mysql_db = "wijesekarapos";
+
+        try {
+            base64.decode_from_base64();
+            File temp_config = new File(fw.getDefaultDirectory() + "\\WPOS\\config_temp.dat");
+            FileReader fr = new FileReader(temp_config);
+
+            BufferedReader br = null;
+            if (temp_config.exists()) {
+                br = new BufferedReader(fr);
+            }
+
+            while ((line = br.readLine()) != null) {
+                mysql_uname = br.readLine();
+                mysql_pass = br.readLine();
+                mysql_server = br.readLine();
+                mysql_db = br.readLine();
+            }
+            //fetching details from the config file
+            fr.close();
+            br.close();
+            temp_config.delete();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Error reading the config file!");
+        }
+        
+        MySqlDBConnect dbconclass = new MySqlDBConnect();
+        dbconclass.getDBFromFile(mysql_server,mysql_db,mysql_uname,mysql_pass);
+        
+    }//GEN-LAST:event_login_btnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -166,7 +283,7 @@ public class LoginWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton configdbBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -175,5 +292,6 @@ public class LoginWindow extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton login_btn;
     // End of variables declaration//GEN-END:variables
 }
